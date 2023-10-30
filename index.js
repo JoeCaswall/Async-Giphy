812a0e9a7718cbb5ae38e8fbd3e91ef80b3a982b
JoeCaswall <joe.caswall@gmail.com>
2023-10-30 14:56:52 +0000

    async function works

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-require('dotenv').config();
+require("dotenv").config();
+
+// const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${proccess.env.API_KEY}&q=dog&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
 
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+// console.log(process.env.API_KEY);
+
+const key = process.env.API_KEY;
+
+// console.log(key);
+async function getImages(query) {
+  try {
+    const response = await fetch(
+      `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
+    );
+    const data = await response.json();
+    let chosenImage = data.data[Math.floor(Math.random() * 24)].url;
+    console.log(chosenImage);
+    return chosenImage;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getImages("spaceman");
